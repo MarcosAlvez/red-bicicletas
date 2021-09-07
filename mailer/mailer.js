246ac4cbc393c2ad8f6c309a0420fe16b33e7d78
Marcos Alvez <76714873+MarcosAlvez@users.noreply.github.com>
2021-09-07 18:45:59 -0300

    Agregado sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,36 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'rickey.ondricka9@ethereal.email',
-        pass: 'vh4SPYAnqzD1NzpxDc'
+let mailConfig;
+
+if (process.env.NODE_ENV === 'production'){
+    const options = {
+        auth: {
+            api_key: process.env.SG_API_SECRET
+        }
     }
-};
+    mailConfig = sgTransport(options);
+}else{
+    if(process.env.NODE_ENV === 'staging'){
+        console.log('***STAGING.ENV***');
+        const options = {
+            auth: {
+                api_key: process.env.SG_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(options);
+    }else {
+        //mails catched by ethereal
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ethereal_usr,
+                pass: process.env.ethereal_pwd
+            }
+        }
+    }
+
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
